194096025ced0354d6f318061c83698d22eea7cc
Zay_Bo_Hein <zay-bo-hein@gmail.com>
2020-12-06 16:06:04 +0630

    ZayBoHein Add name

---
+++ TestGit/src/AddName.java
@@ -0,0 +1,15 @@
+
+public class AddName {
+
+	public static void main(String[] args) {
+		System.out.println(PrintZay());
+		System.out.println();
+		System.out.println();
+	}
+
+	public static String PrintZay() {
+		return "ZayBoHein";
+
+	}
+
+}
